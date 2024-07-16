a45919b0b8ce195a2386d0d70e1dbc733dd58431
thkiaAdiba <athkiaadiba@gmail.com>
2024-07-16 18:12:07 +0600

    added registration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -34,6 +35,58 @@ async function run() {
         const userCollection = database.collection("users");
 
 
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        });
+
+        // Protected route to get user info
+        // app.get('/api/auth/me', async (req, res) => {
+        //     try {
+        //         const user = await db.collection('users').findOne({ _id: new ObjectId(req.decoded.id) });
+        //         res.send(user);
+        //     } catch (err) {
+        //         res.status(500).send({ message: 'Error fetching user info' });
+        //     }
+        // });
+
+
+        app.post('/users', async (req, res) => {
+            const { name, pin, mobileNumber, email } = req.body;
+
+            const query = { email: email }
+            const existing = await userCollection.findOne(query);
+            if (existing) {
+                return res.send({ message: 'User already exist', insertedId: null })
+            }
+
+
+            if (!name || !pin || !mobileNumber || !email) {
+                return res.status(400).send({ message: 'All fields are required' });
+            }
+            if (!/^\d{5}$/.test(pin)) {
+                return res.status(400).send({ message: 'PIN must be a 5-digit number' });
+            }
+
+            const hashedPin = bcrypt.hashSync(pin, 10);
+
+            const newUser = {
+                name,
+                pin: hashedPin,
+                mobileNumber,
+                email,
+                status: 'pending',
+                balance: 0,
+            };
+
+            // console.log(req.headers)
+            // const user = req.body;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
